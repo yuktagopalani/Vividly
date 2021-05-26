64b669ebacef8fa7ad11d2fa4dbd243223cabd9a
yukta <iit2019052@iiita.ac.in>
2021-05-26 19:11:16 +0530

    Sign Out Done

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final _auth = FirebaseAuth.instance;
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: [
+          IconButton(onPressed: (){
+              _auth.signOut();
+              Navigator.pop(context);
+          }, icon: Icon(Icons.logout))
+        ],
+      ),
+    );
   }
 }
